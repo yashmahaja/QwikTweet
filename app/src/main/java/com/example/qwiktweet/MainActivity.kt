5ef4c876820b087f2c5341808f174b8871da65d4
Yash Mahajan <58283608+yashmahaja@users.noreply.github.com>
2024-06-25 22:20:58 -0400

    QwikTweet App

--- app/src/main/java/com/example/qwiktweet/MainActivity.kt
+++ app/src/main/java/com/example/qwiktweet/MainActivity.kt
@@ -5,15 +5,28 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.qwiktweet.api.TweetsyAPI
 import com.example.qwiktweet.ui.theme.QwikTweetTheme
+import com.example.tweetsy.screens.CategoryScreen
+import com.example.tweetsy.screens.DetailScreen
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -23,41 +36,45 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var tweetsyAPI: TweetsyAPI
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        GlobalScope.launch {
-            var response = tweetsyAPI.getCategories()
-            Log.d("Androidddddd",response.body().toString())
-            println(response)
-        }
         enableEdgeToEdge()
         setContent {
-            QwikTweetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            QwikTweetTheme {  Scaffold(
+                topBar = {
+                 TopAppBar(title = {
+                     Text(text = "Tweetsy")
+                 }, Modifier.background(Color.Blue))
                 }
+            ) {
+                Box(modifier = Modifier.padding(it)){
+                    App()
+                }
+            }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QwikTweetTheme {
-        Greeting("Android")
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "category"){
+        composable(route = "category"){
+            CategoryScreen {
+                navController.navigate("detail/${it}")
+            }
+        }
+        composable(route = "detail/{category}",
+            arguments = listOf(
+                navArgument("category"){
+                    type = NavType.StringType
+                }
+            )
+        ){
+            DetailScreen()
+        }
     }
 }
--- app/src/main/java/com/example/qwiktweet/api/TweetsyAPI.kt
+++ app/src/main/java/com/example/qwiktweet/api/TweetsyAPI.kt
@@ -13,6 +13,6 @@ interface TweetsyAPI {
     suspend fun getTweets(@Header("X-JSON-Path") category: String) : Response<List<TweetListItem>>
 
     @GET("/v3/b/6676ee9facd3cb34a85b7df4?meta=false")
-    @Headers("X-JSON-Path: tweets..category")
+    @Headers("X-JSON-Path: tweets..category")   
     suspend fun getCategories(): Response<List<String>>
 }
